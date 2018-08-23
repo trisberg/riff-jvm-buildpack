491997851920ee74ec729a53d0e2e2995bc16e01
trisberg <trisberg@pivotal.io>
2018-08-23 16:22:01 -0400

    Add requirement for riff.toml file
    - pull handler from riff.toml
    
    - use projectriff/mvnfunv3 as the buildpack repository name

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ set -e
 
 docker pull packsdev/v3:run
 
-docker build -t trisberg/packsv3mvn:detect -f Dockerfile.detect .
-docker build -t trisberg/packsv3mvn:build -f Dockerfile.build .
-docker tag packsdev/v3:run trisberg/packsv3mvn:run
+docker build -t projectriff/mvnfunv3:detect -f Dockerfile.detect .
+docker build -t projectriff/mvnfunv3:build -f Dockerfile.build .
 
-echo ./pack build [IMAGENAME] --detect-image trisberg/packsv3mvn:detect
+echo ./pack build [IMAGENAME] --detect-image projectriff/mvnfunv3:detect
