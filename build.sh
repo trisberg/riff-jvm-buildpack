98dc82a5d87e9d753487b665feecf7dbec64584b
trisberg <trisberg@pivotal.io>
2018-08-28 09:31:30 -0400

    Add Gradle buildpack

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 set -e
 
 docker pull packsdev/v3:run
+docker tag packsdev/v3:run projectriff/jvmfunv3:run
 
-docker build -t projectriff/mvnfunv3:detect -f Dockerfile.detect .
-docker build -t projectriff/mvnfunv3:build -f Dockerfile.build .
+docker build -t projectriff/jvmfunv3:detect -f Dockerfile.detect .
+docker build -t projectriff/jvmfunv3:build -f Dockerfile.build .
 
-echo ./pack build [IMAGENAME] --detect-image projectriff/mvnfunv3:detect
+echo ./pack build [IMAGENAME] --detect-image projectriff/jvmfunv3:detect
